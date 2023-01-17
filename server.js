4b2f3b462b4fcc0784b427e51ffc94b576b8d1fb
Aashir Israr <aashirisrar968@gmail.com>
2023-01-17 20:22:46 +0500

    working version

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ app.get("/convert", (req, res) => {
   const options = {
     method: "GET",
     url: "https://currency-converter-by-api-ninjas.p.rapidapi.com/v1/convertcurrency",
-    params: { have: "USD", want: "EUR", amount: "5000" },
+    params: {
+      have: req.query.have,
+      want: req.query.want,
+      amount: req.query.amount,
+    },
     headers: {
       "X-RapidAPI-Key": "b8eb4c64d1msh82f4f5538e5065ap1d2b92jsn7113e96b5efb",
       "X-RapidAPI-Host": "currency-converter-by-api-ninjas.p.rapidapi.com",
@@ -22,7 +26,7 @@ app.get("/convert", (req, res) => {
   axios
     .request(options)
     .then(function (response) {
-      console.log(response.data);
+      res.json(response.data);
     })
     .catch(function (error) {
       console.error(error);
